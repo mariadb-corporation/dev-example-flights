62af02c43e4256339d31d84f061a77b9b3fddbcf
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2020-06-04 07:34:55 -0500

    Updated schema non-usage, commented out ssl usage

--- api/nodejs/db.js
+++ api/nodejs/db.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 // * Remember to change the location of "skysql_chain.pem" to wherever you placed it!
 // * To use just uncomment the two lines below and the 'ssl' property (and value) within the connection pool configuration
 
-const fs = require("fs");
-const serverCert = [fs.readFileSync("skysql_chain.pem", "utf8")];
+//const fs = require("fs");
+//const serverCert = [fs.readFileSync("skysql_chain.pem", "utf8")];
 
 const pool = mariadb.createPool({
   host: process.env.DB_HOST, 
@@ -16,9 +16,11 @@ const pool = mariadb.createPool({
   database: process.env.DB_NAME,
   multipleStatements: true,
   connectionLimit: 5
+  /*
   ,ssl: {
     ca: serverCert
   }
+  */
 });
 
 module.exports={
