53ad9f84a8571f75c995b584613653c2ffb3889f
Rob Hedgpeth <rob.hedgpeth@gmail.com>
2021-07-07 08:33:19 -0500

    Update db.js

--- api/nodejs/db.js
+++ api/nodejs/db.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 // * Remember to change the location of "skysql_chain.pem" to wherever you placed it!
 // * To use just uncomment the two lines below and the 'ssl' property (and value) within the connection pool configuration
 
-const fs = require("fs");
-const serverCert = [fs.readFileSync("skysql_chain.pem", "utf8")];
+//const fs = require("fs");
+//const serverCert = [fs.readFileSync("skysql_chain.pem", "utf8")];
 
 const pool = mariadb.createPool({
   host: process.env.DB_HOST, 
@@ -14,10 +14,11 @@ const pool = mariadb.createPool({
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
-  multipleStatements: true,
+  multipleStatements: true
+  /*,
   ssl: {
     ca: serverCert
-  }
+  }*/
 });
 
 // Expose the Pool object within this module
